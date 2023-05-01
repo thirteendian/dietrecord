4ac1df83eec1f12ccb3140ee505effbb28253218
Yuxuan Yang <yy340@duke.edu>
2023-05-01 19:52:01 -0400

    add auth

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
 import logo from './logo.svg';
-import './App.css';
-
+import '@aws-amplify/ui-react/styles.css';
+import {
+  withAuthenticator,
+  Button,
+  Heading,
+  Image,
+  View,
+  Card,
+} from "@aws-amplify/ui-react";
+import { Auth } from "aws-amplify";
 function App() {
+    const signOut = async () => {
+        try {
+            await Auth.signOut();
+        } catch (error) {
+            console.error("Error signing out: ", error);
+        }
+    };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <View className="App">
+        <Card>
+          <Image src={logo} className="App-logo" alt="logo" />
+          <Heading level={1}>We now have Auth!</Heading>
+        </Card>
+        <Button onClick={signOut}>Sign Out</Button>
+      </View>
   );
 }
 
-export default App;
+export default withAuthenticator(App);
